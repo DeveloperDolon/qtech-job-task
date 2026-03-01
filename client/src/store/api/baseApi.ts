3f2ce8549155859c615373773705232a716741f3
DeveloperDolon <dolonr718@gmail.com>
2026-03-01 13:30:30 +0600

    redux base api and all setup complete

---
+++ client/src/store/api/baseApi.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as any).auth.token;
+
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  tagTypes: ["User"],
+  endpoints: () => ({}),
+});
